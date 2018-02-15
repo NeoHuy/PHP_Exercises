de515e0f7997da75995bf595c3f8797f996f441d
NeoHuy <quanghuy9490@gmail.com>
2018-02-15 07:18:02 +0700

    bt1

--- exercise_1/index.php
+++ exercise_1/index.php
@@ -14,12 +14,31 @@
 
     sort($arrayOne);
     sort($arrayTwo);
-    
-    echo var_dump($arrayOne);
-    echo "<br/>";
-    echo var_dump($arrayTwo);
+
 
     $arr = array_merge($arrayOne, $arrayTwo);
     rsort($arr);
-    echo var_dump($arr);
+
+    function delete_array($value, $array) {
+        if($value != NULL) {
+            $i = 0;
+            $length = count($array);
+            echo "Length of array: " . $length . "<br/>";
+            for(;$i < $length; $i++) {
+                if($array[$i] == $value) {
+                    //array_splice($array, $i, 1);
+                    unset($array[$i]);
+                }
+            }
+            echo "Length of array after delete: " . count($array) . "<br/>";
+            print_r($array);
+        } else {
+            return NULL;
+        }
+    }
+
+    delete_array(1, $arrayOne);
+    echo "<br/>";
+    print_r($arrayOne);
+    
 ?>
