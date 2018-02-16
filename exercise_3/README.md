2f45bd57c1ec89485c37d4b3fa3bbcac973cb834
NeoHuy <quanghuy9490@gmail.com>
2018-02-16 18:07:41 +0700

    Learning to create popup modal

---
+++ exercise_3/README.md
@@ -0,0 +1,18 @@
+# Exercise 3
+
+Tạo 1 popup register user: bao gồm: email, ngày sinh, phone, password
+Validate email (email invalid), phone ( phone VN thôi, tối đa 12 số ), password ( có chữ tối thiểu 1 chữ cái viết hoa, có số và bao gồm 1 ký tự đặc biệt, ít nhất 8 chữ số )
+Ngày sinh theo format: dd-MM-YYYY ( validate date invalid  và trên 18 tuổi)
+Validate front-end và cả back-end
+Yêu cầu thêm: 
+Sau 1 tháng nếu user đó có vào lại thì hiện lại poup ko thì ẩn nó đi
+Có nghĩa là nó đăng ký thành công hoặc tắt popup lần đầu tiên là đã tính timeline
+chứ ko phải nó đăng ký thêm lần nữa hoặc tắt mỡ popup thì gia hạn thêm.
+Cái expire thời gian hiện popup cần được thảo luận thêm
+Đăng ký thành công thì ko cho hiện popup nữa
+Đăng ký thành công thì đến trang my account
+Trang my account thể hiện profile user đó
+ko cho popup hiện ra trên trang my account
+Trang my account có 1 link bắt phải đăng ký mới thấy 
+Còn nếu đăng ký rồi thì thể hiện profile ra
+   
--- exercise_3/index.php
+++ exercise_3/index.php
@@ -2,57 +2,59 @@
 <HTML>
 <head>
     <meta charset="utf8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Homepage</title>
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/latest/normalize.css"/>    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="css/style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,700italic,400italic" rel="stylesheet" />
+    <script type="type/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="type/javascript" src="js\jquery.leanModal.min.js"></script>
 </head>
-
 <body>
-    <div class="container">
-        <h1>Popup register form</h1>
-        <a id="modal_trigger" href="#modal" class="btn">Click here to signup</a>
+    <h2>Modal login form</h2>
+    
+    <button onclick="document.getElementById('id01').style.display = 'block'" style="width:auto;">
+        Login</button>
+    <div id="id01" class="modal">
+        <form class="modal-content animate" method="my_account.php">
+            <div class="imgcontainer">
+                <span onclick="document.getElementById('id01').style.display = 'none'" class="close"
+                title="Close Modal">&times;</span>
+                <img src="img_avatar2.png" alt="Avatar" class="avatar" />
+            </div>
 
-        <div id="modal" class="popupContainer" style="display:none;">
-            <header class="popupHeader">
-                <span class="header_title">Register</span>
-                <span class="modal_close"><i class="fa fa-times"></i></span>
-            </header>
-            
-            <section class="popupBody">
-                <!-- Register form -->
-                <div class="user_register">
-                    <form>
-                        <label>Email</label><input type="text" />
-                        <br/>
+            <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required />
 
-                        <label>Birthday</label><input type="text" />
-                        <br/>
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required />
 
-                        <label>Phone</label><input type="text" />
-                        <br/>
+                <button type="submit">Login</button>
+                <label>
+                    <input type="checkbox" checked="checked" name="remember" /> Remember me
+                </label>
+            </div>
 
-                        <label>Password</label><input type="password" />
-                        <br/>
-
-                        <div class="action_btn">
-                            <a href="#" class="btn btn-red">Register</a>
-                        </div>
-                    </form>
-                </div>
-            </section>
-        </div>
+            <div class="container" style="background-color: #f1f1f1;">
+                <button type="button" onclick="document.getElementById('id01').style.display = 'none'"
+                class="cancelbtn">Cancel</button>
+                <span class="psw">Forgot <a href="#">password?</a></span>
+            </div>
+        </form>
     </div>
     
-    <script type="type/javascript" src="js/jquery.leanModal.min.js"></script>
-    <script type="type/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script>
-        $("#modal_trigger").leanModal({
-            top:100,
-            overlay: 0.6,
-            closeButton: ".modal_close"
-        });
-        
-        $("#user_register").show();
+        //Get the modal
+        var modal = document.getElementById('id01');
 
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if(event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
     </script>
 </body>
 </HTML>
