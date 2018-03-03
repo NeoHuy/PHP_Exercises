2b8cbee246b19e21bc0de19a47587719fb363804
NeoHuy <quanghuy9490@gmail.com>
2018-03-03 18:13:17 +0700

    Validate birthdate in js

--- exercise_3/include_js.php
+++ exercise_3/include_js.php
@@ -2,4 +2,55 @@
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
 <script src="js/jquery.mask.min.js"></script>
-<script src="js/handle.js"></script>
+<script>
+$(document).ready(function() {
+    // jQuery validator
+    $('#modal-form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            birth: {
+                required: true
+            },
+            psw: {
+                required: true,
+                minlength: 8
+            },
+            phone: {
+                maxlength: 12
+            }
+        }
+    });
+
+    jQuery.validator.addMethod("psw", function(value, element) {
+        var specialChars = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+            //console.log("psw: " + element.value);
+        var result = /*value.length >= 6 &&*/ /\d/.test(value) && /[A-Z]/.test(value) && specialChars.test(value);
+        return result;
+    }, "Your password must be contain at least one number, uppercase letter and special character");
+
+    jQuery.validator.addMethod("birth", function(value, element) {
+        var date = Date.now();
+        console.log("Date: " + date);
+        console.log("birthdate: " + element.value);
+        var birthTemp = element.value.split("-");
+        var birthdate = birthTemp[0] + " " + birthTemp[1] + " " + birthTemp[2];
+        birthdate = birthdate.parse();
+        console.log("Birthdate after converted: " + birthdate.parse());
+    }, "");
+
+    //Cant type anything except number
+    $("#phone").mask("0000-00000000");
+    $("#datepicker").mask("00-00-0000");
+});
+
+// Add datepicker to birthday field
+$('#datepicker').datepicker({
+    dateFormat: 'dd-mm-yy'
+});
+
+// Get the modal
+var modal = document.getElementById('id01');
+</script>
--- exercise_3/register_form.php
+++ exercise_3/register_form.php
@@ -1,91 +1,88 @@
 <?php 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+// if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    //Validate email
-    if(empty($_POST['email'])) {
-        $emailErr = "Email is required!";
-    } else {
-        $email = testInput($_POST['email']);
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email format";
-        }
-    }
+//     //Validate email
+//     if(empty($_POST['email'])) {
+//         $emailErr = "Email is required!";
+//     } else {
+//         $email = testInput($_POST['email']);
+//         if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//             $emailErr = "Invalid email format";
+//         }
+//     }
 
-    if(empty($_POST['psw'])) {
-        $pswErr = "Password is required!";
-    } else {
-        $psw = testInput($_POST['psw']);
-        if(count($psw) < 6) {
-            $pswErr = "Password is must more than 5 chars.";
-        }
-    }
+//     if(empty($_POST['psw'])) {
+//         $pswErr = "Password is required!";
+//     } else {
+//         $psw = testInput($_POST['psw']);
+//         if(count($psw) < 6) {
+//             $pswErr = "Password is must more than 5 chars.";
+//         }
+//     }
 
-    if(empty($_POST['birth'])) {
-        $birthErr = "Birthdate is required!";
-    } else {
-        $birthdate = $_POST['birth'];
-        if(!validateAge($birthdate)) {
-            $birthErr = "You must be 18 or older";
-        }
-    }
+//     if(empty($_POST['birth'])) {
+//         $birthErr = "Birthdate is required!";
+//     } else {
+//         $birthdate = $_POST['birth'];
+//         if(!validateAge($birthdate)) {
+//             $birthErr = "You must be 18 or older";
+//         }
+//     }
 
-    if(!empty($_POST['phone'])) {
-        $phone = testInput($_POST['phone']);
-        if(strlen($phone) >= 13) {
-            $phoneErr = "Phone number must be 12 or smaller";
-        }
-    }
+//     if(!empty($_POST['phone'])) {
+//         $phone = testInput($_POST['phone']);
+//         if(strlen($phone) >= 13) {
+//             $phoneErr = "Phone number must be 12 or smaller";
+//         }
+//     }
     
-    setcookie('email', testInput($_POST['email']), $cookieTime, '/');
-    setcookie('psw', testInput($_POST['psw']), $cookieTime, '/');
-    setcookie('birth', testInput($_POST['birth']), $cookieTime, '/');
-    setcookie('phone', testInput($_POST['phone']), $cookieTime, '/');
+//     setcookie('email', testInput($_POST['email']), $cookieTime, '/');
+//     setcookie('psw', testInput($_POST['psw']), $cookieTime, '/');
+//     setcookie('birth', testInput($_POST['birth']), $cookieTime, '/');
+//     setcookie('phone', testInput($_POST['phone']), $cookieTime, '/');
 
-    //redirect to my_account.php after submit
-    //header('Location: my_account.php');
-}
+//     //redirect to my_account.php after submit
+//     //header('Location: my_account.php');
+// }
 
-function validateAge($birthdate, $age = 18) {
-    if(is_string($birthdate)) {
-        $birthdate = strtotime($birthdate);
-    }
-    if(time() - $birthdate < $age * 31536000) {
-        return false;
-    }
-    return true;
-}
+// function validateAge($birthdate, $age = 18) {
+//     if(is_string($birthdate)) {
+//         $birthdate = strtotime($birthdate);
+//     }
+//     if(time() - $birthdate < $age * 31536000) {
+//         return false;
+//     }
+//     return true;
+// }
 
-function testInput($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+// function testInput($data) {
+//     $data = trim($data);
+//     $data = stripslashes($data);
+//     $data = htmlspecialchars($data);
+//     return $data;
+// }
 ?>
 
 <div id="id01" class="modal">
-    <form class="modal-content animate" method="post" action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <form class="modal-content animate" id="modal-form" method="post" action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <div class="imgcontainer">
+            <h2 class="form-title">Sign Up</h2>
             <span onclick="document.getElementById('id01').style.display = 'none'"
             class="close" title="Close Modal">&times;</span>
         </div>
 
         <div class="container">
-            <label for="email"><b>Email (required)</b></label>
+            <label for="email"><b>Email</b></label>
             <input type="text" placeholder="Enter Email" name="email">
-            <span><?php echo $emailErr; ?></span>
 
-            <label for="psw"><b>Password (required)</b></label>
-            <input type="password" placeholder="Enter Password" name="psw">
-            <span><?php echo $pswErr; ?></span>
+            <label for="psw"><b>Password</b></label>
+            <input type="password" class="psw" placeholder="Enter Password" name="psw">
 
-            <label for="birth"><b>Date of birth (required)</b></label>
-            <input type="text" name="birth" id="datepicker">
-            <span><?php echo $birthErr; ?></span>
+            <label for="birth"><b>Date of birth</b></label>
+            <input type="text" class="birth" name="birth" id="datepicker">
             
-            <label for="phone"><b>Phone number (optional)</b></label>
-            <input type="text" id="phone" name="phone" maxLength=12>
-            <span><?php echo $phoneErr; ?></span>
+            <label for="phone"><b>Phone number</b></label>
+            <input type="text" id="phone" name="phone">
 
             <button type="submit" name="submit">Register</button>
         </div>
